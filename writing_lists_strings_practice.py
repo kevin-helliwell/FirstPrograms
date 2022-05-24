be4719bcec859405c6f28c40c5939f54a1350154
Kevin Helliwell <kbh789@gmail.com>
2022-05-24 09:41:14 -0400

    list and average

--- writing_lists_strings_practice.py
+++ writing_lists_strings_practice.py
@@ -1,6 +1,10 @@
-my_list = [1, 2, 3]
+my_list = []
+for i in range(20):
+    response = float(input("Please enter a number.\n"))
+    my_list.append(response)
+
 if len(my_list) == 0:
     exit()
 elif len(my_list) != 0:
     average = sum(my_list) / len(my_list)
-    print(average)
+    print(f"The average is {average}")
